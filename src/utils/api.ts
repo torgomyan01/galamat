edb8fcb8d0f5ca8243143604fba74d5d46df6d5e
Andranik Torgomyan <and.torgomyan01@gmail.com>
2025-05-31 23:25:24 +0400

    Fix bugs

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,5 @@
 import instance from "../../axios-config";
-import { API_ENDPOINTS } from "@/utils/consts";
 
 export const GetProducts = () => {
-  return instance.get(API_ENDPOINTS.PRODUCTS.LIST);
+  return instance.get("/products");
 };
