da5023a8eeea643c3576b0b9d502def7efbf847f
Andranik Torgomyan <and.torgomyan01@gmail.com>
2025-06-22 18:49:37 +0400

    Fix bugs

--- src/app/admin/projects/page.tsx
+++ src/app/admin/projects/page.tsx
@@ -1,16 +1,10 @@
-// app/admin/projects/page.tsx
-
-"use server";
-
 import Projects from "@/app/admin/projects/projects";
 import { fetchHouses } from "@/lib/getHouses";
 import { ActionGetProjectsInfo } from "@/app/actions/projects/get-projects";
 
-// 🔁 Ասում ենք՝ cache չպահել, միշտ server-side render
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-  // 🔁 Կատարում ենք երկու async հարցում միաժամանակ
   const [housesData, housesDataAdmin] = await Promise.all([
     fetchHouses(),
     ActionGetProjectsInfo(),
