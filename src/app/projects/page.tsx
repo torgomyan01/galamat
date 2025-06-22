723e6f5fa9b731d03a9a311fc09e65e395cd73c2
Andranik Torgomyan <and.torgomyan01@gmail.com>
2025-06-22 17:26:56 +0400

    Fix bg

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,16 +1,19 @@
 "use server";
 
 import React from "react";
-import { fetchHouses } from "@/lib/getHouses";
 import Projects from "@/components/layout/projects/projects";
 import { ActionGetProjectsInfo } from "@/app/actions/projects/get-projects";
+import { GetHouses } from "@/utils/api";
 
 async function Page() {
-  const housesData: any = await fetchHouses();
+  const housesData: any = await GetHouses({});
   const housesDataAdmin: any = await ActionGetProjectsInfo();
 
   return (
-    <Projects houses={housesData} housesDataAdmin={housesDataAdmin.data} />
+    <Projects
+      houses={housesData.data.data}
+      housesDataAdmin={housesDataAdmin.data}
+    />
   );
 }
 
