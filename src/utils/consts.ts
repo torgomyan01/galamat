72207a9f5df61ef3865abfc922c75a1f3745d67e
Andranik Torgomyan <and.torgomyan01@gmail.com>
2025-07-20 13:03:30 +0400

    sss

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -3,7 +3,6 @@ export const servLink = "https://pb14286.profitbase.ru/api/v4/json";
 export const filesLink = "https://s.galamat.kz";
 export const filesLinkSave = `${filesLink}/save-images.php`;
 export const filesLinkRemove = `${filesLink}/remove-image.php`;
-
 export const SITE_URL = {
   HOME: "/",
   REQUESTS: "/requests",
