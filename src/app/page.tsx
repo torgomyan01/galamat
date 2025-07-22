1c449b6c887371236cc0ec90909ea195c3b363d3
Andranik Torgomyan <and.torgomyan01@gmail.com>
2025-07-22 21:43:07 +0400

    ddd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,18 @@
 
 import React from "react";
 import Home from "@/components/layout/home/home";
-import { fetchHouses } from "@/lib/getHouses";
-import { ActionGetProjectsInfo } from "@/app/actions/projects/get-projects";
-import { mergeComplexesWithProjects } from "@/utils/helpers";
 
 async function Page() {
-  const [housesData, housesDataAdmin] = await Promise.all([
-    fetchHouses(),
-    ActionGetProjectsInfo(),
-  ]);
-  const mergeProjectProfitDb: IProjectMerged[] = mergeComplexesWithProjects(
-    housesData,
-    housesDataAdmin.data as any,
-  ).filter((project) => !project.hide);
+  // const [housesData, housesDataAdmin] = await Promise.all([
+  //   fetchHouses(),
+  //   ActionGetProjectsInfo(),
+  // ]);
+  // const mergeProjectProfitDb: IProjectMerged[] = mergeComplexesWithProjects(
+  //   housesData,
+  //   housesDataAdmin.data as any,
+  // ).filter((project) => !project.hide);
 
-  return <Home projects={mergeProjectProfitDb} />;
+  return <Home />;
 }
 
 export default Page;
