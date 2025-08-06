8e722376c95733577d9a9c15ea677175427d6dba
Andranik Torgomyan <and.torgomyan01@gmail.com>
2025-08-07 00:16:34 +0400

    Fix bugs

--- src/components/common/footer/footer.tsx
+++ src/components/common/footer/footer.tsx
@@ -7,32 +7,40 @@ function Footer() {
   const $t = useTranslate();
 
   return (
-    <footer className="footer">
+    <footer className="footer mb-[30px] md:pb-[50px]">
       <div className="wrapper">
-        <ul className="footer-menu">
-          <li>
-            <Link href={SITE_URL.CONTACT}>{$t("contacts__")}</Link>
-          </li>
-          <li>
-            <Link href={SITE_URL.OUR_COMPANY}>{$t("about_company")}</Link>
-          </li>
-          <li>
-            <Link href={`${SITE_URL.HOME}#leave-request`}>
-              {$t("leave_a_request")}
-            </Link>
-          </li>
-          <li>
-            <Link href={SITE_URL.USER_AGREEMENT}>{$t("user_agreement")}</Link>
-          </li>
-        </ul>
         <div className="footer-info">
           <Link href={SITE_URL.HOME} className="footer-logo min-w-[197px]">
             <img src="img/footer-logo.svg" alt="" />
           </Link>
-          <p>{$t("all_materials_posted")}</p>
-          <a href="#" className="private">
-            {$t("privacy_policy__")}
-          </a>
+
+          <div className="flex-je-c gap-5 mt-6 md:mt-0">
+            <Link href="#">
+              <i className="fa-brands fa-youtube text-[35px] text-blue"></i>
+            </Link>
+            <Link href="#">
+              <i className="fa-brands fa-instagram text-[30px] text-blue"></i>
+            </Link>
+            <Link href="#">
+              <i className="fa-brands fa-tiktok text-[25px] text-blue"></i>
+            </Link>
+            <Link href="#">
+              <i className="fa-brands fa-whatsapp text-[30px] text-blue"></i>
+            </Link>
+          </div>
+        </div>
+        <div>
+          <div className="flex-jc-c md:flex-js-c gap-4 md:gap-6 mb-6 flex-col md:flex-row">
+            <Link href="#" className="private underline text-blue">
+              {$t("privacy_policy__")}
+            </Link>
+            <Link href="#" className="private underline text-blue">
+              {$t("about_company")}
+            </Link>
+          </div>
+          <p className="text-[#5B75A4] text-[13px] hidden md:block">
+            {$t("all_materials_posted")}
+          </p>
         </div>
       </div>
     </footer>
