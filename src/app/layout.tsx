feb9d8093e1232e296fa83f4e6f8a9f7eec0995a
torgomyan01 <and.torgomyan01@gmail.com>
2025-08-08 16:56:15 +0400

    Fix bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ import NextTopLoader from "nextjs-toploader";
 
 import { Providers } from "@/app/providers";
 
-import { GoogleAnalytics } from "@next/third-parties/google";
 import YandexMetrika from "@/components/common/YandexMetrika/YandexMetrika";
 
 export async function generateMetadata() {
@@ -47,13 +46,33 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="ru" suppressHydrationWarning={true}>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KTS8G3K9');`,
+          }}
+        />
+      </head>
       <body>
         <NextTopLoader />
         <Providers>{children}</Providers>
 
-        <GoogleAnalytics gaId="AW-10990855116" />
+        {/*<GoogleAnalytics gaId="AW-10990855116" />*/}
 
         <YandexMetrika />
+
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KTS8G3K9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
       </body>
     </html>
   );
